359556d0e105420e3508db192d39fa8d34df84aa
Samuel Falcon <saxclar1025@gmail.com>
2018-03-13 20:39:10 -0400

    linked reset, style, bootstrap, jquery, and firebase, and added firebase initialization

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyD117KRn5q_2rOItAYV5Coej9TpwDCNcvw",
+    authDomain: "wtf-todo-5b04d.firebaseapp.com",
+    databaseURL: "https://wtf-todo-5b04d.firebaseio.com",
+    projectId: "wtf-todo-5b04d",
+    storageBucket: "",
+    messagingSenderId: "726104455374"
+  };
+firebase.initializeApp(config);
+var database = firebase.database();
